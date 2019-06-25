a7cdb9e4e5048cd6da34b4582a43ff2a0b88759d
ezcire <ezcire@gmail.com>
2019-06-24 22:05:51 -0500

    done

--- client3.py
+++ client3.py
@@ -2,23 +2,7 @@ import socket
 import sys
 import argparse
 import threading
-
-def receive(socket, signal):
-    while signal:
-        try:
-            data = socket.recv(32)
-            print(str(data.decode("utf-8")))
-            q = str(data.decode("utf-8"))
-            if q == 'q':
-                print("disconnet\n")
-                serverSocket.close()
-                signal = False
-                break
-        except:
-            print("You have been disconnected from the server")
-            signal = False
-            break
-
+import http.client
 
 parser = argparse.ArgumentParser()
 parser.add_argument("Host_Name")
@@ -26,36 +10,45 @@ parser.add_argument("PortNumber", nargs = '?' , type = int, default = 8080)
 parser.add_argument("filename" , nargs = '?', default = "index.html")
 args = parser.parse_args()
 
-#host = socket.gethostname()
 host = args.Host_Name
 port = args.PortNumber
-print("hostname is ", host)
-print("port is ", port)
 
+def receive(socket, signal):
+    while signal:
+        try:
+            data = socket.recv(4096)
+            print('Hostname\t\t: ', socket.getpeername())
+            print('Socket family\t\t: ', serverSocket.family) 
+            print('Socket protocol\t\t: ', serverSocket.proto)
+            print('Socket timeout\t\t: ', serverSocket.gettimeout())
+            print('Socket type\t\t: ', serverSocket.type)
+            print(data)
+            serverSocket.close()  
+            signal = False                
+        except:
+            print("You have been disconnected from the server")
+            socket.close()
+            signal = False
+            break
 
 try:
     serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     serverSocket.connect((host,port))
     try:
-        print(args.filename)
-        serverSocket.sendall(str.encode(args.filename))
-    except:
+        send_file = "GET /" + args.filename + " HTTP/1.1\r\n\r\n"    
+        serverSocket.send(str.encode(send_file))
+        
+    except IOError:
         print("could not send filename")
-        serverSocket.close()    
+        serverSocket.close()
+                
 except:
     print("Sorry could not connect")
     input("Press enter to quit")
     sys.exit(0)
+   
 
 receive_thread = threading.Thread(target = receive, args = (serverSocket, True))
 receive_thread.start()
 
-while True:
-    print("Enter filename\nPress q to quit")
-    message = input()
-    if message == 'q':
-        serverSocket.sendall(str.encode(message))
-        input("Press enter to quit")
-        serverSocket.close()
-        break
-    serverSocket.sendall(str.encode(message))
+    
--- server3.py
+++ server3.py
@@ -3,11 +3,23 @@ import argparse
 import threading
 import requests
 import sys
+import http.client
+import os
+
 
 #get input from terminal as parse 
 #if no PORT input, set to default 8080
 parser = argparse.ArgumentParser()
 parser.add_argument("PortNumber", nargs = '?' , type = int, default = 8080)
+args = parser.parse_args()
+
+serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+getPort = parser.parse_args()
+port = getPort.PortNumber
+   #print("port is ", port) #print for me to see
+serverSocket.bind(("localhost",port))
+serverSocket.listen(5)
 
 connections_array = []  #store clients in arrays
 connections_length = 0
@@ -23,48 +35,35 @@ class add_client(threading.Thread):
         self.signal = signal 
         
     def __str__(self):
-        return str(self.id) + " " + str(self.address)
+        return str(self.id) + " " + str(self.address) + " " + str(self.socket)
 
     def run(self):
         while self.signal:
             try:
-                data = self.socket.recv(32)
-                q = str(data.decode("utf-8"))
-                if q == 'q':
-                    print("Client " + str(self.address) + " has disconnected")
-                    self.signal = False
-                    connections_array.remove(self)
-                    break
+                data = self.socket.recv(1024)
             except:
                 print("Client " + str(self.address) + " has disconnected")
                 self.signal = False
                 connections_array.remove(self)
-                #self.socket.close()
                 break
             if data != "":
                 #open file
                 try:
-                    print("ID " + str(self.id) + ": ")
-                    filename = str(data.decode("utf-8"))
-                    print("filename is ",filename)
-                    f = open(filename, "r")
-                    print(f.read())
-                    print("\n\n")
-                    f.close()  
-                    #response = requests.get(filename)
-                    #print(response.status_code)
-                    print("Ready to serve..")
+                    message = data
+                    fixed_filename = message.split()[1]
+                    f = open(fixed_filename[1:]) 
+                    out = f.read()
+                   #send message back to client
+                    self.socket.sendall(bytes("HTTP/1.1 200 OK\r\n\r\n", "UTF-8"))
+                    self.socket.sendall(bytes(out, "UTF-8"))
+                   #close the client and file 
+                    f.close()    
+                    self.socket.close()
+    
                     
                 except IOError:
-                    print("404 oops could not read file")
-                    print("\n\n")
-                    print("Ready to serve..")
-                        
-            for client in connections_array:
-                    if client.id != self.id: 
-                        client.socket.sendall(data)
-                         
-                            
+                    self.socket.send(bytes("HTTP/1.1 404 Not Found\r\n\r\n", "utf-8"))
+                    self.socket.close()
 
 def new_connection(socket):
     while True:
@@ -73,20 +72,14 @@ def new_connection(socket):
         global connections_length
         connections_array.append(add_client(connectionSocket, addr, connections_length, "Name", True))
         connections_array[len(connections_array) - 1].start()
-        print("New client " + str(connections_array[len(connections_array) - 1]))
+        print("New client " )#+ str(connections_array[len(connections_array) - 1]))
         connections_length += 1 
-    connectionSocket.close()
-def main():
-    serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    
 
-    getPort = parser.parse_args()
-    port = getPort.PortNumber
-   #print("port is ", port) #print for me to see
-    serverSocket.bind(("localhost",port))
-    serverSocket.listen(1)
+add_new_thread = threading.Thread(target = new_connection, args = (serverSocket,))
+add_new_thread.start()
 
-    add_new_thread = threading.Thread(target = new_connection, args = (serverSocket,))
-    add_new_thread.start()
-main()   
+  
+  
 
 
